db08b2d556f95bb3b49badfa129df2123fbba755
Amat Alsalam saleh <53934332+salama-eng@users.noreply.github.com>
2021-03-09 07:47:22 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function openModal() {
+	'use strict';
+	
+	document.getElementById("myOverlay").style.display = "block";
+}
+function closeModal() {
+	'use strict';
+				
+	document.getElementById("myOverlay").style.display = "none";
+}
+function openModal1() {
+	'use strict';
+	
+	document.getElementById("myOverlay1").style.display = "block";
+}
+function closeModal1() {
+	'use strict';
+				
+	document.getElementById("myOverlay1").style.display = "none";
+}
